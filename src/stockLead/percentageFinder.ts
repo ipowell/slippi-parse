7f2ccfd9f3dfb1f8a31c1b090f97350bc4e7f999
Ian <ipowell15@gmail.com>
2022-12-17 17:41:48 -0500

    Initial stock lead work.

---
+++ src/stockLead/percentageFinder.ts
@@ -0,0 +1,32 @@
+import stockData from "../../stockData.json";
+
+function createEmptyPercentageResults(): String[][] {
+  return [
+    ["", "", "", "", ""],
+    ["", "", "", "", ""],
+    ["", "", "", "", ""],
+    ["", "", "", "", ""],
+    ["", "", "", "", ""],
+  ];
+}
+
+function findPercentages(
+  winnerData: number[][],
+  loserData: number[][]
+): String[][] {
+  const results: String[][] = createEmptyPercentageResults();
+  for (let i = 0; i < 5; i++) {
+    for (let j = 0; j < 5; j++) {
+      let wins = winnerData[i][j];
+      let losses = loserData[i][j];
+      results[i][j] = (wins / (wins + losses)).toPrecision(3);
+    }
+  }
+  return results;
+}
+
+const falcoWins = stockData.Falco.Peach;
+const peachWins = stockData.Peach.Falco;
+
+const percents = findPercentages(falcoWins, peachWins);
+console.log(percents);
